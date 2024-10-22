a572c248e279fcc83fd1b27ccc4f382d04d6c2c7
princenedjoh <62966723+princenedjoh@users.noreply.github.com>
2024-10-22 11:06:33 +0000

    added cors, added jwt authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,31 @@ import { StreamModule } from './stream/stream.module';
 import { ConfigModule } from '@nestjs/config';
 import { CameraFolderModule } from './camera folder/camFolder.module';
 import { UserModule } from './user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './guards/jwt.guards';
 
 @Module({
   imports: [
     StreamModule,
     CameraFolderModule,
     UserModule,
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get, HttpException, NotFoundException, Param } from "@nestjs/common";
+import { Controller, Get, HttpException, NotFoundException, Param, UseGuards } from "@nestjs/common";
 import { Stream, User } from "@prisma/client";
 import { UserService } from "./user.service";
 import { ApiTags } from "@nestjs/swagger";
+import { JwtAuthGuard } from "src/guards/jwt.guards";
+import { User as IUser } from "src/decorators/User.decorator";
+import { JwtPayload } from "src/types/@types";
 
 @ApiTags("User")
 @Controller('user')
 export class UserController{
     constructor(private userService: UserService) {}
 
+    @UseGuards(JwtAuthGuard)
     @Get('getUserDetails/:id')
-    getAllStreams(@Param("id") id : string) : Promise<User[]>{
+    getAllStreams(@Param("id") id : string, @IUser() user : JwtPayload) : Promise<User[]>{
+        console.log({user})
         try {
             return this.userService.getUserDetails(id)
         } catch (error) {
